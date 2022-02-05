45d144e401f6834f5ede94fd92c7c0b021c97839
Celestyn <celestynkinny3@gmail.com>
2022-02-05 17:36:38 +0530

    Application Commit

---
+++ app/src/main/java/com/NumCo/numberconverter/CipherCreation/CipherDialogFragmentAdapter.java
@@ -0,0 +1,35 @@
+package com.NumCo.numberconverter.CipherCreation;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class CipherDialogFragmentAdapter extends FragmentStateAdapter {
+
+    private final ArrayList<Fragment> cipherDialogFragments = new ArrayList<>();
+
+    public CipherDialogFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return cipherDialogFragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cipherDialogFragments.size();
+    }
+
+    public void addCipherDialogFragment(Fragment dialogFragment) {
+        cipherDialogFragments.add(dialogFragment);
+    }
+}
